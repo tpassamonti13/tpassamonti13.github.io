05e9b65676cdbf6936b462a0690e651453313924
tpassamonti13 <tpassamonti13@gmail.com>
2019-09-29 08:25:09 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -12,4 +12,12 @@ $(document).ready(function()
 
 function retrieveStats()
 {
+  $.ajax({
+    url: 'https://api.fortnitetracker.com/v1/profile/pc/Tony.P.21',
+    dataType: json,
+    type: "GET",
+    success: function(data)
+    {
+      console.log(data);
+    });
 }
