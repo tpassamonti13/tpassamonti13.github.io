94167fc8de2a97e45da6186926d49ce25db3c5aa
tpassamonti13 <tpassamonti13@gmail.com>
2019-09-29 12:20:08 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ $(document).ready(function()
 function retrieveStats()
 {
   $.ajax({
-    url: 'https://api.fortnitetracker.com/v1/profile/{pc}/{Tony.P.21}',
+    url: 'http://cors.io/?https://api.fortnitetracker.com/v1/profile/{pc}/{Tony.P.21}',
     headers: 
     {
       'Access-Control-Allow-Origin' : '*',
