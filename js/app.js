a1eba93e1a64af7c8af18614d9ba646fe666663a
tpassamonti13 <tpassamonti13@gmail.com>
2019-09-29 12:27:04 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -12,19 +12,11 @@ $(document).ready(function()
 
 function retrieveStats()
 {
-  $.ajax({
-    url: 'https://cors.io/?https://api.fortnitetracker.com/v1/profile/{pc}/{Tony.P.21}',
-    headers: 
-    {
-      'Access-Control-Allow-Origin' : '*',
-      'TRN-Api-Key' : '3f4fd021-940a-4cee-b3b1-bf4fcc4a29e9',
-    },
-    crossDomain: true,
-    dataType: "json",
-    type: "GET",
-    success: function(data)
-    {
-      console.log(data);
-    }
-  });
+  var xhttp = new XMLHttpRequest();
+  xhttp.request("GET", "https://cors.io/?https://api.fortnitetracker.com/v1/profile/{pc}/{Tony.P.21}", true);
+  xhttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+  xhttp.setRequestHeader('Access-Control-Allow-Headers', '*');
+  xhttp.setRequestHeader('Access-Control-Allow-Methods', '*');
+  xhttp.setRequestHeader('TRN-Api-Key' "3f4fd021-940a-4cee-b3b1-bf4fcc4a29e9");
+  xhttp.send();
 }
