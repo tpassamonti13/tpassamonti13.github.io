fa73af76e5c75bf454885c5bb28a306a24bf974c
tpassamonti13 <tpassamonti13@gmail.com>
2019-09-29 08:19:41 -0400

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var winStatSession;
+var killStatSession;
+var kdStatSession;
+var winStatLifetime;
+var killStatLifetime;
+var kdStatLifetime;
+
+$(document).ready(function()
+{
+
+});
+
+function retrieveStats()
+{
+}
