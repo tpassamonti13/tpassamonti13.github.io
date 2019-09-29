6f23e8cc0b3aacc0c86624b95b9aba2828a11b2c
tpassamonti13 <tpassamonti13@gmail.com>
2019-09-29 08:41:29 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -16,6 +16,7 @@ function retrieveStats()
     url: 'https://api.fortnitetracker.com/v1/profile/pc/Tony.P.21',
     headers: 
     {
+      'Access-Control-Allow-Origin' : '*',
       'TRN-Api-Key': '3f4fd021-940a-4cee-b3b1-bf4fcc4a29e9'
     },
     dataType: "json",
