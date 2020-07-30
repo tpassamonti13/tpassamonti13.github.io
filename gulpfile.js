d2a94aa75e3605d284fe808e13e797d6fb5b2076
tpassamonti13 <anthony.passamonti@caci.com>
2020-07-30 11:47:51 -0400

    Initial add of Royalty RGB website files.

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+//compile scss into css
+function style() {
+    return gulp.src('app/scss/**/*.scss')
+    .pipe(sass().on('error',sass.logError))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.stream());
+}
+function watch() {
+    browserSync.init({
+        server: {
+           baseDir: "./app",
+           index: "/index.html"
+        }
+    });
+    gulp.watch('app/scss/**/*.scss', style)
+    gulp.watch('./*.html').on('change',browserSync.reload);
+    gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+}
+exports.style = style;
+exports.watch = watch;
